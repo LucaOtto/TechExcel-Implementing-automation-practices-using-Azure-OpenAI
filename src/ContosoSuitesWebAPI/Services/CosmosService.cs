fef080ba675b798858683b0f94ef69e44e302717
LucaOtto <42404744+LucaOtto@users.noreply.github.com>
2024-02-22 14:13:58 +0000

    modified

--- src/ContosoSuitesWebAPI/Services/CosmosService.cs
+++ src/ContosoSuitesWebAPI/Services/CosmosService.cs
@@ -15,28 +15,49 @@ public class CosmosService : ICosmosService
     public CosmosService()
     {
         _client = new CosmosClient(
-            connectionString: Environment.GetEnvironmentVariable("AZURE_COSMOS_DB_CONNECTION_STRING")!
+            connectionString: "AccountEndpoint=https://2zyyzzxzl56gu-cosmosdb.documents.azure.com:443/;AccountKey=BD9oN4zRKBJUrfYLXo00InwTFugua20DOTjEbbJLlX3SQO4e9U6Ium6fwpX4m0nlA7XsmB4mUFuDACDbagBoQg==;"
         );
     }
 
     public async Task<IEnumerable<Customer>> GetCustomersByName(string name)
     {
-        throw new NotImplementedException();
+        var queryable = container.GetItemLinqQueryable<Customer>();
+        using FeedIterator<Customer> feed = queryable
+            .Where(c => c.FullName == name)
+            .ToFeedIterator<Customer>();
+        return await ExecuteQuery(feed);
     }
 
     public async Task<IEnumerable<Customer>> GetCustomersByLoyaltyTier(string loyaltyTier)
     {
-        throw new NotImplementedException();
+        LoyaltyTier lt = Enum.Parse<LoyaltyTier>(loyaltyTier);
+        var queryable = container.GetItemLinqQueryable<Customer>();
+        using FeedIterator<Customer> feed = queryable
+            .Where(c => c.LoyaltyTier.ToString() == loyaltyTier)
+            .ToFeedIterator<Customer>();
+        return await ExecuteQuery(feed);
     }
 
     public async Task<IEnumerable<Customer>> GetCustomersWithStaysAfterDate(DateTime dt)
     {
-        throw new NotImplementedException();
+        var queryable = container.GetItemLinqQueryable<Customer>();
+        using FeedIterator<Customer> feed = queryable
+            .Where(c => c.DateOfMostRecentStay > dt)
+            .ToFeedIterator<Customer>();
+        return await ExecuteQuery(feed);
     }
 
     private async Task<IEnumerable<Customer>> ExecuteQuery(FeedIterator<Customer> feed)
     {
-        throw new NotImplementedException();
+        List<Customer> results = new();
+        while (feed.HasMoreResults)
+        {
+            var response = await feed.ReadNextAsync();
+            foreach (Customer c in response)
+            {
+                results.Add(c);
+            }
+        }
+        return results;
     }
-    
 }
